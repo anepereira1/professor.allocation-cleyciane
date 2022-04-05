c82eb2e44f2e2d5190b4e9bbba7b1af65ca8e8f8
Ane Pereira <cleycianepereira1@gmail.com>
2022-04-05 19:49:04 -0300

    Add initial SpringBoot Web Project files

---
+++ src/main/java/com/project/professorallocation/ProfessorAllocationCleycianeApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationCleycianeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationCleycianeApplication.class, args);
+	}
+
+}
