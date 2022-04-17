f08f6a7fd8e4c11eccc61d6a620afe1b52141577
Ane Pereira <cleycianepereira1@gmail.com>
2022-04-17 00:02:37 -0300

    Add Allocation Repository tests

--- src/test/java/com/project/professorallocation/repository/AllocationRepositoryTests.java
+++ src/test/java/com/project/professorallocation/repository/AllocationRepositoryTests.java
@@ -1,5 +1,59 @@
 package com.project.professorallocation.repository;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestPropertySource;
+
+import com.project.professorallocation.model.Allocation;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+@TestPropertySource(locations = "classpath:application.properties")
 public class AllocationRepositoryTests {
 
+	private SimpleDateFormat sdf = new SimpleDateFormat("HH:mmZ");
+
+	@Autowired
+	private AllocationRepository repository;
+
+	@Test
+	public void create() throws ParseException {
+		Allocation allocation = new Allocation();
+		allocation.setDayOfWeek(DayOfWeek.TUESDAY);
+		allocation.setStartHour(sdf.parse("19:00-0300"));
+		allocation.setEndHour(sdf.parse("21:00-0300"));
+		allocation.setProfessorId(1L);
+		allocation.setCourseID(1L);
+
+		allocation = repository.save(allocation);
+
+		System.out.println(allocation);
+	}
+
+	@Test
+	public void findAll() {
+		List<Allocation> allAllocations = repository.findAll();
+
+		allAllocations.forEach(System.out::println);
+
+	}
+
+	@Test
+	public void findAllByProfessor() {
+
+		List<Allocation> allocationsOfASpecificProfessor = repository.findByProfessorId(1L);
+
+		allocationsOfASpecificProfessor.forEach(System.out::println);
+
+	}
 }
